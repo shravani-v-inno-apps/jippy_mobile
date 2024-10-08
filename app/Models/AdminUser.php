1959b2c5aaa4c1b2729075bd8c61d6909bfca73e
naresh-k-inno-apps <naresh.k@innoappsindia.com>
2024-10-08 15:03:11 +0530

    test

---
+++ app/Models/AdminUser.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminUser extends Model
+{
+    use HasFactory;
+
+    protected $table = 'st_admin_user';
+
+    protected $fillable = [
+        'admin_id',
+        'admin_id_token',
+        'username',
+        'password',
+        'first_name',
+        'last_name',
+        'email_address',
+        'contact_number',
+        'profile_photo',
+        'path',
+        'role',
+        'main_account',
+        'session_token', 
+        'status',
+        'last_login',
+        'date_created',
+        'date_modified',
+        'ip_address'
+    ];
+
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\ShravaniController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\MobileController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,3 +22,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/country-codes', [MobileController::class, 'getCountryCodes']);
